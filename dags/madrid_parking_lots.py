81317ffa510a1f9e43fd5f12b49e87021c885b9e
RuthNavarroCarrasco <ruth.navarro@nextdigital.es>
2025-04-05 14:32:57 +0200

    fix: HTTPOperator

--- dags/madrid_parking_lots.py
+++ dags/madrid_parking_lots.py
@@ -1,24 +1,29 @@
-from airflow.decorators import dag
+from airflow import DAG
 from airflow.providers.http.operators.http import HttpOperator
 from datetime import datetime
 
-@dag(
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+}
+
+with DAG(
     dag_id='madrid_parking_lots',
-    start_date=datetime(2024, 1, 1),
+    default_args=default_args,
     schedule_interval='@yearly',
     catchup=False,
-    tags=['example'],
-)
-def madrid_parking_lots_dag():
+    tags=['madrid', 'api'],
+) as dag:
+
     get_calles_ser = HttpOperator(
-        task_id="get_op",
+        task_id="get_ser_calles",
         method="GET",
-        http_conn_id=None,
-        endpoint="get",
-        data={"title": "SER-calles", "fmt": "json"},
-        headers={},
+        http_conn_id="datos_madrid_http",
+        endpoint="egob/catalogo/title/SER-calles.json",
+        headers={"Accept": "application/json"},
+        log_response=True,
     )
 
     get_calles_ser
-
-madrid_parking_lots_dag()
